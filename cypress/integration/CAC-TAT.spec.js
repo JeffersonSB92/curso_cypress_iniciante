dfb670d0585f2c32974a7df4e89b89e36f565d9c
Jefferson Borges <jeffersondasilvaborges92@gmail.com>
2023-08-29 19:57:57 -0300

    taken .only

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -255,7 +255,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .click()
     })
 
-    it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
         cy.get('#privacy a')
             .invoke('removeAttr', 'target')
             .click()
